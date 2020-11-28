8e2a3d51bbd488c52acf6de53249330443eced26
Ankurjyoti Das <68739425+AnkurJDas171@users.noreply.github.com>
2020-11-28 23:35:47 +0530

    changes made

--- src/components/townsComponent.js
+++ src/components/townsComponent.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Towns({ inputState}) {
   return (
     <>
-      {inputState.towns.map((town) => (
+      {inputState.towns.map((town,index) => (
         <div key={town.name} id={`town${index+1}`}>{town.name}</div>
       ))}
     </>
